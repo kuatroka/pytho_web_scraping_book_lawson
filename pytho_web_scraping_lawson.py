d89de11376c7b09c7c07620993fcd9ac7acef9f3
kuatroka <aaarmani_2000@yahoo.com>
2019-12-11 00:15:45 +0000

    start moving download() to a class

--- pytho_web_scraping_lawson.py
+++ pytho_web_scraping_lawson.py
@@ -1365,7 +1365,99 @@ class CsvCallback:
 
             self.writer.writerow(all_rows)
 
-link_crawler('http://example.webscraping.com/places/default/view/United-Kingdom-239', '/(index|view)',max_depth=1, scrape_callback=CsvCallback())
+link_crawler('http://example.webscraping.com/places/default/view/United-Kingdom-239', '/(index|view)',max_depth=-1, scrape_callback=CsvCallback())
 ## be careful with parameters for link_regex and regex in re.search as if they are not correct and do not correspond to the 
 ## real data on the page, it will give index our range error or something else
 # CsvCallback().writer.close() 
+
+
+####################################################################################
+### Adding cache to the crawler
+### we will refactor the download fn to a class and use the throttle functionality in it to 
+### so we only throttle when downloading is made and not when the loading from cache
+
+from random import choice
+import requests
+
+class Downloader:
+        """ Downloader class to use cache and requests for downloading pages.
+        For contructor, pass:
+            delay (int): # of secs delay between requests (default: 5)
+            user_agent (str): user agent string (default: 'wswp')
+            proxies (list[dict]): list of possible proxies, each
+                must be a dict with http / https keys and proxy values
+            cache (dict or dict-like obj): keys: urls, values: dicts with keys (html, code)
+            timeout (float/int): number of seconds to wait until timeout
+        """
+
+    def __init__(self, delay=5, user_agent='wswp', proxies=None, cache={}):
+        self.throttle = Throttle(delay)
+        self.user_agent = user_agent
+        self.proxies = proxies
+        self.num_retries = None # we'll set it per request
+        self.cache = cache
+
+    def __call__(self, url, num_retries=2):
+        """ Call the downloader class, which will return HTML from cache
+            or download it
+            args:
+                url (str): url to download
+            kwargs:
+                num_retries (int): # times to retry if 5xx code (default: 2)
+        """
+        self.num_retries = num_retries
+        try:
+            result = self.cache[url]
+            print('Loaded from cache:', url)
+
+        except KeyError:
+            result = None
+        if result and self.num_retries and 500 <= result['code'] < 600:
+            # server error so ignore result from cache
+            # and re-download
+            result = None
+
+        if result is None:
+            # result was not loaded from cache
+            self.throttle.wait(url)
+            proxies = choice(self.proxies) if self.proxies else None
+            headers = {'User-Agent': self.user_agent}
+            result = self.download(url, headers, proxies)
+            self.cache[url] = result
+
+        return result['html']
+
+
+    def download(self,url, headers, proxies):
+        """ Download a and return the page content
+            args:
+                url (str): URL
+                headers (dict): dict of headers (like user_agent)
+                proxies (dict): proxy dict w/ keys 'http'/'https', values
+                    are strs (i.e. 'http(s)://IP') (default: None)
+        """
+        print('Downloading:', url)
+
+        try:
+            resp = requests.get(url, headers=headers, proxies=proxies)
+
+            html = resp.text
+            if resp.status_code >= 400:
+                print('Download error:', resp.text)
+                html = None
+                if num_retries and 500 <= resp.status_code < 600:
+                    # recursively retry 5xx HTTP errors
+                    self.num_retries -= 1
+                    return self.download(url, headers, proxies)
+
+        except requests.exception.RequestException as e:
+            print('Download Error:', e)
+            return {'html': None, 'code': 500}
+
+        return {'html': html, 'code': resp.status_code}
+
+        
+
+
+            
+
